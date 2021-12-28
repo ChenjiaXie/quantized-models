b05795fa8bab0acf5514d8651fc0cc54f10f854d
adreamxcj <2236746281@qq.com>
2021-12-28 15:51:19 +0800

    modify README

--- README.md
+++ README.md
@@ -28,3 +28,23 @@ ghostnet            : https://1drv.ms/u/s!Ahqo_6nBJPIHhloNb-Rg2uXs38MU?e=Smakww
 
 # Run instructions
 python validate.py --data <imagenet foder location> --model <modelname> --actbit <8 or 16 for ghostnet>
+
+    # mobilenet_v3_large resnet_18 googlenet shufflenet_v2 inception_v3
+
+    # python validate.py --model mobilenet_v3_large --data ./imagenet
+
+    # python validate.py --model resnet_18 --data ./imagenet
+
+    # python validate.py --model resnet_50 --data ./imagenet
+
+    # python validate.py --model resnext_101 --data ./imagenet
+
+    # python validate.py --model googlenet --data ./imagenet
+
+    # python validate.py --model shufflenet_v2 --data ./imagenet
+
+    # python validate.py --model inception_v3 --data ./imagenet
+
+    # python validate.py --model mobilenet_v2 --data ./imagenet
+
+    # python validate.py --model ghostnet --data ./imagenet --actbit 8
--- eval.sh
+++ eval.sh
@@ -1,14 +1,10 @@
-# rm -r result/
-# mkdir result
 # mobilenet_v3_large resnet_18 googlenet shufflenet_v2 inception_v3
-# python validate.py --model mobilenet_v3_large --data /home/xiechenjia/imagenet
-# python validate.py --model resnet_18 --data /home/xiechenjia/imagenet
-# python validate.py --model resnet_50 --data /home/xiechenjia/imagenet
-# python validate.py --model resnext_101 --data /home/xiechenjia/imagenet
-# python validate.py --model googlenet --data /home/xiechenjia/imagenet
-# python validate.py --model shufflenet_v2 --data /home/xiechenjia/imagenet
-python validate.py --model inception_v3 --data /home/xiechenjia/imagenet
-# python validate.py --model mobilenet_v2 --data /home/xiechenjia/imagenet
-# python validate.py --model ghostnet --data /home/xiechenjia/imagenet --actbit 8
-# python comp_rate.py 
-
+# python validate.py --model mobilenet_v3_large --data ./imagenet
+# python validate.py --model resnet_18 --data ./imagenet
+# python validate.py --model resnet_50 --data ./imagenet
+# python validate.py --model resnext_101 --data ./imagenet
+# python validate.py --model googlenet --data ./imagenet
+# python validate.py --model shufflenet_v2 --data ./imagenet
+python validate.py --model inception_v3 --data ./imagenet
+# python validate.py --model mobilenet_v2 --data ./imagenet
+# python validate.py --model ghostnet --data ./imagenet --actbit 8
