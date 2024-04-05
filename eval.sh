f6ff5dc7345bae419b4510f95e42f8799e91f2ab
adreamxcj <50480220+Adreamxcj@users.noreply.github.com>
2024-04-05 15:47:43 +0800

    fremove useless info

--- eval.sh
+++ eval.sh
@@ -2,7 +2,7 @@ log_path=./LOGS
 wt_path=./qmodels
 ### models: vgg_* resnet_* convnext_* mobilenet_* efficientnet_* inception_v3 shufflenet_* googlenet
 model=convnext_tiny
-data_path=/data-hdd/xiechenjia/DATASETS/CNN_DATASETS/imagenet-1k
+data_path=your_imagenet_path
 
 mkdir -p $log_path
 mkdir -p $wt_path
